dffa871f4a7817c5096dc02b3318a0d9a80c33b2
fawda123 <mbafs2012@gmail.com>
2015-01-02 11:27:03 -0600

    data processing with qaqc as zero
    daily max of cumprcp for daily aggs

--- R/dat_proc.R
+++ R/dat_proc.R
@@ -25,43 +25,67 @@ foreach(stat = stats) %dopar% {
   # import raw
   tmp <- import_local(path, stat)
   
-  # qaqc
-  tmp <- qaqc(tmp, qaqc_keep = c(5, 4, 0))
-  
-  # treat nuts differently
+  # nut processing
   if(grepl('nut$', stat)){
     
     tmp <- rem_reps(tmp)
     
-    # assign tmp to object, save, clear memory
-    assign(stat, tmp)
-    save(list = stat, file = paste0('M:/wq_models/SWMP2/proc1/', stat, '.RData'))
-    save(list = stat, file = paste0('M:/wq_models/SWMP2/proc2/', stat, '.RData'))
-    rm(list = stat)
-    rm('tmp')
+    tmp <- qaqc(tmp, qaqc_keep = c(0, 4, 5))
+    
+    tmp_agg <- tmp
+    
+  }
   
-  # wq, met processing
-  } else {
+  # met processing
+  if(grepl('met$', stat)){
   
     # setstep to 15
     tmp <- setstep(tmp)
     
+    # keep passed, historical, corrected for all except totprcp
+    # keep only passed for totprcp\
+    totprcp <- subset(tmp, select = 'totprcp')
+    totprcp <- qaqc(totprcp, qaqc_keep = 0)
+    tmp <- qaqc(tmp, qaqc_keep = c(0))
+    tmp$totprcp <- totprcp$totprcp
+    
     # aggregate by days
     tmp_agg <- aggregate(tmp, by = 'days')
     
-    # assign tmp to object, save, clear memory
-    assign(stat, tmp)
-    save(list = stat, file = paste0('M:/wq_models/SWMP2/proc1/', stat, '.RData'))
-    rm(list = stat)
-    rm('tmp')
-    
-    # assign tmp_agg to object, save, clear memory
-    assign(stat, tmp_agg)
-    save(list = stat, file = paste0('M:/wq_models/SWMP2/proc2/', stat, '.RData'))
-    rm(list = stat)
-    rm('tmp_agg')
+    # use daily max for cumprcp
+    cumprcp <- aggregate(tmp, by = 'days', 
+      FUN = function(x) max(x, na.rm = T), 
+      params = 'cumprcp')
+    cumprcp$cumprcp[cumprcp$cumprcp == -Inf] <- NA_real_
+    tmp_agg$cumprcp <- cumprcp$cumprcp
   
   }
   
+  # wq processing
+  if(grepl('wq$', stat)){
+    
+    # setstep to 15
+    tmp <- setstep(tmp)
+    
+    # keep passed, historical, corrected for all except totprcp
+    tmp <- qaqc(tmp, qaqc_keep = c(0, 4, 5))
+    
+    # aggregate by days
+    tmp_agg <- aggregate(tmp, by = 'days')
+    
+  }
+    
+  # assign tmp to object, save, clear memory
+  assign(stat, tmp)
+  save(list = stat, file = paste0('M:/wq_models/SWMP2/proc1/', stat, '.RData'))
+  rm(list = stat)
+  rm('tmp')
+  
+  # assign tmp_agg to object, save, clear memory
+  assign(stat, tmp_agg)
+  save(list = stat, file = paste0('M:/wq_models/SWMP2/proc2/', stat, '.RData'))
+  rm(list = stat)
+  rm('tmp_agg')
+
 }
 stopCluster(cl)
